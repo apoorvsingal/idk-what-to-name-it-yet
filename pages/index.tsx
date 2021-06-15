eabc0e0c82c388648a9581283575120497688bf5
Cow <69002970+k9chelsea2@users.noreply.github.com>
2021-06-15 02:02:17 +0000

    too tired to finish it

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,47 @@ const Index = () => {
 				</nav>
 				<hero className="flex w-full h-2/4">
 				</hero>
+				<section className="flex items-start flex-col sm:flex-row justify-center sm:justify-evenly bg-secondaryPrimaryLight text-secondaryDarkr p-6 sm:p-10 gap-2 sm:gap-4">
+					<article className="w-full">
+						<h3 className="text-xl font-medium sm:text-3xl pb-0.5 sm:py-2">What is kaow?</h3>
+						<p className="font-
+						extralight text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</article>
+
+					<article className="w-full">
+						<h3 className="text-xl font-medium sm:text-3xl pb-0.5 sm:py-2">Why should I use Kaow?</h3>
+						<p className="font-
+						extralight text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</article>
+
+					<article className="w-full">
+						<h3 className="text-xl font-medium sm:text-3xl pb-0.5 sm:py-2">How do I get
+						started!</h3>
+						<p className="font-
+						extralight text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</article>
+				</section>
+
+				<section className="flex items-start flex-col sm:flex-row justify-center sm:justify-evenly text-secondaryPrimaryLight bg-secondaryDark p-6 sm:p-10 gap-2 sm:gap-4">
+
+					<article className="w-full">
+						<h3 className="text-xl font-medium sm:text-3xl pb-0.5 sm:py-2">Placeholder</h3>
+						<p className="font-
+						extralight text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</article>
+
+					<article className="w-full">
+						<h3 className="text-xl font-medium sm:text-3xl pb-0.5 sm:py-2">Placeholder</h3>
+						<p className="font-
+						extralight text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</article>
+
+					<article className="w-full">
+						<h3 className="text-xl font-medium sm:text-3xl pb-0.5 sm:py-2">Placeholder</h3>
+						<p className="font-
+						extralight text-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					</article>
+				</section>
 			</main>
 		</>
 	)
